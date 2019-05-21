e0734857fa3cfd72b1e2490bb3983e3bc488c805
Vinton Teo <taulago@ymail.com>
2019-05-20 22:41:56 -0600

    Create tests for Turn class. 2/2 tests pass successfully.

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,31 +4,38 @@ import spies from 'chai-spies';
 chai.use(spies);
 import domUpdates from '../src/domUpdates.js';
 import Game from '../src/Game.js';
+import Round from '../src/Round.js';
 import Turn from '../src/Turn.js';
 import Player from '../src/Player.js';
 import Wheel from '../src/Wheel.js';
 import Data from '../src/Data.js'
 // chai.spy.on(domUpdates, 'createPlayers', () => true);
 
-describe.skip('Turn', function () {
+describe('Turn', function () {
   let names;
   let wheel = new Wheel();
   let game;
+  let turn;
+
   beforeEach(function () {
     names = ['Steve', 'Vinton', 'Jacqueline'];
     wheel.createWheel();
-    game = new Game();
+    game = new Game(wheel);
     game.createPlayers(names);
     game.assignPuzzleBlock();
     game.start();
     game.currentRound.newTurn();
+    turn = game.currentRound.currentTurn;
   });
 
   it('should have default properties', function () {
-    expect(game.currentRound.currentTurn.currentScore).to.equal(0);
+    expect(turn.currentScore).to.equal(0);
+    expect(turn.round).to.be.an.instanceOf(Round);
+    expect(turn.player).to.be.an.instanceOf(Player);
   });
 
   it('Should be able to spin the wheel', function() {
-    expect(game.currentRound.currentTurn.spinWheel()).to.eql();
+    let result = turn.round.game.wheel.values;
+    expect(result).to.include(turn.spinWheel());
   });
 })
